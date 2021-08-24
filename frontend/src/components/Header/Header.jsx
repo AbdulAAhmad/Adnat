8f2c2508229fa27fbaf7922c90024728990b5850
Abdul Ahmad <abdul.as.ahmad@gmail.com>
2021-08-25 09:39:33 +1000

    Add files for Header, Login and Signup components

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+  return (
+    <nav className="header">
+      <h1>Adnat</h1>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./login.css";
+
+const Login = () => {
+  return <div className="login-page"></div>;
+};
+
+export default Login;
