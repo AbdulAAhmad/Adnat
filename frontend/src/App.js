6fb76237192fee1f2af0dfbb607740c7478c8b06
Abdul Ahmad <abdul.as.ahmad@gmail.com>
2021-08-25 19:47:03 +1000

    Add initial routing for testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import { AuthProvider } from "./context/auth/auth.context";
+
+import Header from "./components/Header/Header";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 const App = () => {
   return (
-    <div >
-          The basic empty template
-    </div>
-  )
-}
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
