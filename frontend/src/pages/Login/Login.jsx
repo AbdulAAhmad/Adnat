01a03c71e7ec910a726d6cc0b2ce0207f1585120
Abdul Ahmad <abdul.as.ahmad@gmail.com>
2021-08-25 13:25:19 +1000

    Implement layout for Signup and Login pages

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
 const Login = () => {
-  return <div className="login-page"></div>;
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <div className="login-page">
+      <h1>Log in</h1>
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(formValues);
+        }}
+      >
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={(e) =>
+            setFormValues((prev) => ({ ...prev, email: e.target.value }))
+          }
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={(e) =>
+            setFormValues((prev) => ({ ...prev, password: e.target.value }))
+          }
+        />
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signup.css";
 
 const Signup = () => {
-  return <div className="signup-page"></div>;
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+
+  return (
+    <div className="signup-page">
+      <h1>Sign up</h1>
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={(e) =>
+            setFormValues((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={(e) =>
+            setFormValues((prev) => ({ ...prev, email: e.target.value }))
+          }
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={(e) =>
+            setFormValues((prev) => ({ ...prev, password: e.target.value }))
+          }
+        />
+
+        <label htmlFor="passwordConfirmation">Password confirmation</label>
+        <input
+          id="passwordConfirmation"
+          name="passwordConfirmation"
+          type="password"
+          onChange={(e) =>
+            setFormValues((prev) => ({
+              ...prev,
+              passwordConfirmation: e.target.value,
+            }))
+          }
+        />
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
