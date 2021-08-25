22ba4193b15b0430f904fe62e49141c1d3761181
Abdul Ahmad <abdul.as.ahmad@gmail.com>
2021-08-25 23:44:34 +1000

    Implement conditional logout button and display user name

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./header.css";
+
+import { useAuthDispatch, useAuthState } from "../../context/auth/auth.context";
+import { useUserState, useUserDispatch } from "../../context/user/user.context";
+import { logout } from "../../context/auth/auth.actions";
+import { clearUser } from "../../context/user/user.actions";
+
 const Header = () => {
+  const { sessionId } = useAuthState();
+  const authDispatch = useAuthDispatch();
+  const userDispatch = useUserDispatch();
+  const { name } = useUserState();
+
+  const handleClick = () => {
+    logout(authDispatch);
+    clearUser(userDispatch);
+  };
+
   return (
-    <nav className="header">
+    <div className="header">
       <h1>Adnat</h1>
-    </nav>
+      {sessionId && name && (
+        <div>
+          {name}
+          <button onClick={handleClick}>Log Out</button>
+        </div>
+      )}
+    </div>
   );
 };
 
