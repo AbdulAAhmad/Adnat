354eb24864bd6103ec3196696f91e89d6d051a43
Abdul Ahmad <abdul.as.ahmad@gmail.com>
2021-08-28 19:20:23 +1000

    Add cors dependency to allow cors

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRouter = require("./router/auth");
@@ -5,6 +6,8 @@ const organisationsRouter = require("./router/organisations");
 const shiftsRouter = require("./router/shifts");
 const usersRouter = require("./router/users");
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use("/auth", authRouter);
